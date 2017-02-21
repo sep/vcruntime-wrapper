4c1f44592ace7505a16e68d84f37601119e7d3af
rmnowadly <rmnowadly@sep.com>
2017-02-20 21:16:57 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# vcruntime-wrapper
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: vcruntime-wrapper
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
